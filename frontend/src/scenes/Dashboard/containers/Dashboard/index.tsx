da99eb0d4974c462f93888a16c3775cf8bde86c6
AlexanderTirik <sasha.tirik@gmail.com>
2021-01-20 19:47:06 +0200

    start making dashboard

---
+++ frontend/src/scenes/Dashboard/containers/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.sass';
+
+const Dashboard = () => (
+  <>
+    <header className={`d-flex justify-content-center ${styles.header}`}>Puckwheat Barser</header>
+  </>
+);
+
+export default Dashboard;
